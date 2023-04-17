1772f5dcab6f9390f564c82230e0dac43d06d5d3
PrajayVK <prajay827742@gmail.com>
2023-04-17 20:50:38 +0530

    Addition Updated

--- microservices/addition/app/addition.py
+++ microservices/addition/app/addition.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, request, flash, jsonify
-from flask_cors import CORS
+from flask import Flask
+import flask.scaffold
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+
+from flask_restful import Resource, Api
+class Addition(Resource): 
+	def get(self, num1, num2):
+		return {'result': int(num1) + int(num2)}
+
 app = Flask(__name__)
-CORS(app)
-app.secret_key = "secret_key"
+api = Api(app)
+api.add_resource(Addition, '/<num1>/<num2>')
 
-@app.get("/<int:n1>&<int:n2>")
-def add(n1, n2):
-    return jsonify({"result":n1+n2})
-
-if __name__=="__main__":
-    app.run(debug=True, port=5051, host="0.0.0.0")
+if __name__ =="__main__":
+	app.run(
+		debug=True,
+		port=5051,
+		host="0.0.0.0"
+	)
--- microservices/landing/app/app.py
+++ microservices/landing/app/app.py
@@ -1,49 +1,105 @@
-from flask import Flask, render_template, request, flash
-from flask_cors import CORS
+from flask import Flask, render_template, request, flash, redirect, url_for
+
 import requests
+import os
 
 app = Flask(__name__)
-CORS(app)
-app.secret_key = "secret_key"
+app.secret_key = 'thisisjustarandomstring'
 
-@app.route('/', methods=["GET", "POST"])
+
+def add(n1, n2):
+    URL = 'http://addition-service:'
+    port = 5051
+    add_url = URL + str(port) + '/' + str(n1) + '/' + str(n2)
+    response = requests.get(add_url)
+    print(response)
+    return response.json()['result']
+
+def minus(n1, n2):
+    URL = 'http://subtraction-service:'
+    port = 5052
+    add_url = URL + str(port) + '/' + str(n1) + '/' + str(n2)
+    response = requests.get(add_url)
+    print(response)
+    return response.json()['result']
+
+def multiply(n1, n2):
+    URL = 'http://multiplication-service:'
+    port = 5053
+    add_url = URL + str(port) + '/' + str(n1) + '/' + str(n2)
+    response = requests.get(add_url)
+    print(response)
+    return response.json()['result']
+
+def divide(n1, n2):
+    URL = 'http://division-service:'
+    port = 5054
+    add_url = URL + str(port) + '/' + str(n1) + '/' + str(n2)
+    response = requests.get(add_url)
+    print(response)
+    return response.json()['result']
+
+def greater_than(n1, n2):
+    URL = 'http://greater_than-service:'
+    port = 5055
+    add_url = URL + str(port) + '/' + str(n1) + '/' + str(n2)
+    response = requests.get(add_url)
+    print(response)
+    return response.json()['result']
+    # return n1>n2
+
+def less_than(n1, n2):
+    URL = 'http://less_than-service:'
+    port = 5056
+    add_url = URL + str(port) + '/' + str(n1) + '/' + str(n2)
+    response = requests.get(add_url)
+    print(response)
+    return response.json()['result']
+    # return n1<n2
+
+def equal(n1, n2):
+    URL = 'http://equal-service:'
+    port = 5057
+    add_url = URL + str(port) + '/' + str(n1) + '/' + str(n2)
+    response = requests.get(add_url)
+    print(response)
+    return response.json()['result']
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == "POST":
-        first = request.form.get("first")
-        second = request.form.get("second")
-        operation = request.form.get("operation")
-        
-        # Fix variable type
-        try:
-            first = int(first)
-            second = int(second)
-        except (ValueError, TypeError):
-            flash("Invalid input. Please enter valid integer values.")
-            return render_template("index.html")
-
-        # Handle None type exception
-        if first is None or second is None:
-            flash("Invalid input. Please enter valid integer values.")
-            return render_template("index.html")
-        
+    try:
+        number_1 = int(request.form.get("first"))
+        number_2 = int(request.form.get('second'))
+        operation = request.form.get('operation')
         result = 0
-        if operation == "add":
-            response = requests.get(f"http://addition:5051/{first}&{second}")
-            data = response.json()
-            result = data["result"]
-        elif operation == "minus":
-            result = first - second
-        elif operation == "multiply":
-            result = first * second
-        elif operation == "divide":
-            if second == 0:
-                flash("Invalid input. Cannot divide by zero.")
-                return render_template("index.html")
-            result = first / second
+        if operation == 'add':
+            result = add(number_1, number_2)
+        elif operation == 'minus':
+            result =  minus(number_1, number_2)
+        elif operation == 'multiply':
+            result = multiply(number_1, number_2)
+        elif operation == 'divide':
+            if number_2==0:
+                        result = 'Zero Division Error'
+            else:
+                        result = divide(number_1, number_2)
+        elif operation == 'greater_than':
+            result = greater_than(number_1, number_2)
+        elif operation == 'less_than':
+            result = less_than(number_1, number_2)
+        elif operation == 'equal':
+            result = equal(number_1, number_2)
 
-        flash(f"The result of {operation} operation is {result}.")
-        
-    return render_template("index.html")
+        flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+
+        return render_template('index.html')
+    except:
+        flash(f'No input value')
+        return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5050, debug=True)
+    app.run(
+        debug=True,
+        port=5050,
+        host="0.0.0.0"
+    )
